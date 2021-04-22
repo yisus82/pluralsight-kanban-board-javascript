7e76e87233329ebb2dd9a8652e5a8cbc9685506b
yisus82 <djalma8@gmail.com>
2021-04-22 10:22:23 +0200

    Kanban board events

--- js/kanban.js
+++ js/kanban.js
@@ -9,14 +9,49 @@ add_btn.addEventListener('click', () => {
   const target = document.querySelector('#requested');
   if (adding == false) {
     adding = true;
-    target.appendChild(create_item());
+    target.append(create_item());
   } else {
     error.innerHTML = message;
   }
 });
 
 const create_item = () => {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.id = 'item-' + order;
+  item.draggable = true;
+  item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
+  const save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = '';
+    if (input.value !== '') {
+      order++;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  });
+  const input = document.createElement('input');
+  input.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+      save_btn.click();
+    }
+  });
+  item.appendChild(input);
+  item.appendChild(save_btn);
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', event => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener('dragover', event => {
+    event.preventDefault();
+  });
 });
